5a461679c2bef6527d4fc4750233483ab17b64f8
Thomas Immel <thomas.immel@de.ibm.com>
2025-11-18 17:03:58 +0100

    add HTTP POST calls and JSON reposonse parsing

--- homepageAmpel.ino
+++ homepageAmpel.ino
@@ -1,9 +1,10 @@
-
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 #include "wifiSettings.h"
 
 #define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
@@ -13,23 +14,31 @@
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // define schaffbar states
-#define CLOSED 0
-#define PENDING 1
-#define OPEN 2
+#define RED 0
+#define YELLOW 1
+#define RED_YELLOW 2
+#define GREEN 3
+
+const String SERVER_URL = "http://schaffbar-bb.de/wp-json/ampel/v1/status?status=";
+const String RED_TEXT = "red";
+const String RED_YELLOW_TEXT = "redyellow";
+const String GREEN_TEXT = "green";
+const String YELLOW_TEXT = "yellow";
 
 #define CLOSE_BUTTON_PIN 1
-#define PENDING_BUTTON_PIN 2
-#define OPEN_BUTTON_PIN 3
+#define YELLOW_BUTTON_PIN 2
+#define GREEN_BUTTON_PIN 3
 #define CLOSE_LED_PIN 21
-#define PENDING_LED_PIN 20
-#define OPEN_LED_PIN 10
+#define YELLOW_LED_PIN 20
+#define GREEN_LED_PIN 10
 
-int buttonStateClosed = LOW;  // variable for reading the pushbutton status
-int buttonStatePending = LOW;  // variable for reading the pushbutton status
-int buttonStateOpen = LOW;  // variable for reading the pushbutton status
-int status = CLOSED;  // variable for reading the pushbutton status
+int buttonStateRED = LOW;  // variable for reading the pushbutton status
+int buttonStateYELLOW = LOW;  // variable for reading the pushbutton status
+int buttonStateGREEN = LOW;  // variable for reading the pushbutton status
+int status = RED;  // variable for reading the pushbutton status
 
 WiFiClient wifiClient;
+HTTPClient http;
 String wifiStatus = "";
 
 void setup() {
@@ -37,13 +46,13 @@ void setup() {
 
   //Buttons
   pinMode(CLOSE_BUTTON_PIN, INPUT);
-  pinMode(PENDING_BUTTON_PIN, INPUT);
-  pinMode(OPEN_BUTTON_PIN, INPUT);
+  pinMode(YELLOW_BUTTON_PIN, INPUT);
+  pinMode(GREEN_BUTTON_PIN, INPUT);
 
   //LEDs
   pinMode(CLOSE_LED_PIN, OUTPUT);
-  pinMode(PENDING_LED_PIN, OUTPUT);
-  pinMode(OPEN_LED_PIN, OUTPUT);
+  pinMode(YELLOW_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
 
   delay(250); // wait for the OLED to power up
   display.begin(i2c_Address, true); // Address 0x3C default
@@ -55,40 +64,42 @@ void setup() {
 
   connectToWifi();
 
-  // Initial state = CLOSED
+  // Initial state = RED
   digitalWrite(CLOSE_LED_PIN, HIGH);
-  updateStatus(CLOSED);
+  updateStatus(RED);
 }
 
 void loop() {
-  buttonStateClosed = digitalRead(CLOSE_BUTTON_PIN);
-  if (buttonStateClosed == HIGH) {
+  buttonStateRED = digitalRead(CLOSE_BUTTON_PIN);
+  if (buttonStateRED == HIGH) {
     // turn LED on:
     digitalWrite(CLOSE_LED_PIN, HIGH);
-    updateStatus(CLOSED);
+    updateStatus(RED);
     // turn others off
-    digitalWrite(PENDING_LED_PIN, LOW);
-    digitalWrite(OPEN_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, LOW);
+    digitalWrite(GREEN_LED_PIN, LOW);
   } 
 
-  buttonStatePending = digitalRead(PENDING_BUTTON_PIN);
-  if (buttonStatePending == HIGH) {
+  buttonStateYELLOW = digitalRead(YELLOW_BUTTON_PIN);
+  if (buttonStateYELLOW == HIGH) {
+    //TODO: set LEDs correct
+    
     // turn LED on:
-    digitalWrite(PENDING_LED_PIN, HIGH);
-    updateStatus(PENDING);
+    digitalWrite(YELLOW_LED_PIN, HIGH);
+    updateStatus(YELLOW);
     // turn others off
     digitalWrite(CLOSE_LED_PIN, LOW);
-    digitalWrite(OPEN_LED_PIN, LOW);
+    digitalWrite(GREEN_LED_PIN, LOW);
   } 
 
-  buttonStateOpen = digitalRead(OPEN_BUTTON_PIN);
-  if (buttonStateOpen == HIGH) {
+  buttonStateGREEN = digitalRead(GREEN_BUTTON_PIN);
+  if (buttonStateGREEN == HIGH) {
     // turn LED on:
-    digitalWrite(OPEN_LED_PIN, HIGH);
-    updateStatus(OPEN);
+    digitalWrite(GREEN_LED_PIN, HIGH);
+    updateStatus(GREEN);
     // turn others off
     digitalWrite(CLOSE_LED_PIN, LOW);
-    digitalWrite(PENDING_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, LOW);
   }
 
   delay(100);
@@ -102,29 +113,25 @@ void loop() {
 
 void updateStatus(int requestedStatus){
   String message = "Request status change to: ";
-  message.concat(requestedStatus);
+  switch (requestedStatus){
+    case RED:
+      message.concat(String(RED_TEXT));
+      break;
+    case GREEN:
+      message.concat(String(GREEN_TEXT));
+      break;
+    case YELLOW:
+      message.concat(String(YELLOW_TEXT));
+      break;
+    case RED_YELLOW:
+      message.concat(String(RED_YELLOW_TEXT));
+      break;
+  }
   
   printMessageToDisplay(2, message);
   
-  switch (requestedStatus) {
-    case CLOSED:
-      message = "Schaffbar geschlossen";
-      status = CLOSED;
-      break;
-    case PENDING:
-      if (status == CLOSED) {
-        message = "Schaffbar oeffnet bald";
-      }
-      if (status == OPEN) {
-        message = "Schaffbar schliesst bald";
-      }
-      //status = PENDING;
-      break;
-    case OPEN:
-      message = "Schaffbar offen";
-      status = OPEN;
-      break;
-  }
+  message = sendStatusToWebServer(requestedStatus);
+  status = requestedStatus;
  
   printMessageToDisplay(2, message);
 }
@@ -159,4 +166,64 @@ void connectToWifi(){
     wifiStatus = "Wifi connected. ";
     printMessageToDisplay(1,wifiStatus + WiFi.status());
   }
+}
+
+String sendStatusToWebServer(int status) {
+  String returnMessage = "";
+  String fullServerURL = "";
+  JsonDocument doc;
+
+  switch (status){
+    case RED:
+      fullServerURL = SERVER_URL + RED_TEXT;
+      break;
+    case GREEN: 
+      fullServerURL = SERVER_URL + GREEN_TEXT;
+      break;
+    case YELLOW:
+      fullServerURL = SERVER_URL + YELLOW_TEXT;
+      break;
+    case RED_YELLOW:
+      fullServerURL = SERVER_URL + RED_YELLOW_TEXT;
+      break;
+  }
+
+  Serial.print("Connect to: ");
+  Serial.println(fullServerURL);
+  http.begin(wifiClient, fullServerURL.c_str());
+
+  http.addHeader("x-api-key",apiKey);
+  int httpResponseCode = http.POST("");
+  
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    Serial.println(payload);
+
+    //parse the payload
+    DeserializationError error = deserializeJson(doc, payload);
+    if (!error) {
+      Serial.println("Payload parsed sucessful");
+      const char* parsedMessage = doc["message"];
+      returnMessage = String(parsedMessage);
+      Serial.print("Parsed message: ");
+      Serial.println(returnMessage);
+    } else {
+      Serial.println("Parse payload failed");
+      returnMessage = "Parse payload failed";
+    }
+
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+    returnMessage = "Failed HTTP call: " + String(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+
+
+  return returnMessage;
 }
